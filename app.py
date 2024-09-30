137cae7c6731b6d757dd35b8b3829fa1cc6cec28
sankeerth-k <160320760+sankeerth-k@users.noreply.github.com>
2024-09-30 16:56:05 +0530

    updatein app.py

--- app.py
+++ app.py
@@ -96,3 +96,6 @@ def predict():
     except Exception as e:
         return render_template('index.html', error=str(e))
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no port is provided
+    app.run(host='0.0.0.0', port=port)
